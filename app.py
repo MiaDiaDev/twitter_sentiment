527b64f136868ef31e20b9e323b3a47cd0742e20
MiaDiaDev <56690662+MiaDiaDev@users.noreply.github.com>
2020-02-28 15:45:37 +0100

    updated names

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-import sys, tweepy, csv, re
+import csv, re
+import tweepy
 from textblob import TextBlob
 
 
 class TweetLoader:
     def __init__(self):
         self.tweets = []
-        self.tweetText = []
+        self.tweet_text = []
 
     def download_tweets(self):
-        # authenticating
-        consumerKey = "3gNr8ySpbERuZpVuvd7U9Y73p"
-        consumerSecret = "PO2W76yFwMlPMt3eIllZjd4QFXsh0BQLx97YTCXkmVDIOx58gl"
-        accessToken = "1201141846510067714-M68oypMwk2bqSQVILHRmlS5bFXnBrh"
-        accessTokenSecret = "Lo1iObuulmA7ESRZ89oXjoXqXafnUws1HOTkd1S2Vsd0a"
-        auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-        auth.set_access_token(accessToken, accessTokenSecret)
+        # Authentifikation
+        consumer_key = "3gNr8ySpbERuZpVuvd7U9Y73p"
+        consumer_secret = "PO2W76yFwMlPMt3eIllZjd4QFXsh0BQLx97YTCXkmVDIOx58gl"
+        access_token = "1201141846510067714-M68oypMwk2bqSQVILHRmlS5bFXnBrh"
+        access_token_secret = "Lo1iObuulmA7ESRZ89oXjoXqXafnUws1HOTkd1S2Vsd0a"
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
 
-        # input for term to be searched and how many tweets to search
-        searchTerm = input("Enter Keyword/Tag to search about: ")
-        NoOfTerms = int(input("Enter how many tweets to search: "))
+        # auch andere Suchanfragen einbeziehen?
+        search_term = "#ExtinctionRebellion"
+        quantity_tweets = 300
 
-        # searching for tweets
-        self.tweets = tweepy.Cursor(api.search, q=searchTerm, lang="en").items(
-            NoOfTerms
+        #
+        self.tweets = tweepy.Cursor(api.search, q=search_term, lang="en").items(
+            quantity_tweets
         )
 
         # Open/create a file to append data to
-        csvFile = open("result.csv", "a")
+        csv_file = open("results.csv", "a")
 
         # Use csv writer
-        csvWriter = csv.writer(csvFile)
+        csv_writer = csv.writer(csv_file)
 
         i = 0
         for tweet in self.tweets:
@@ -38,12 +39,13 @@ class TweetLoader:
             if i >= 50:
                 break
             # Append to temp so that we can store in csv later. I use encode UTF-8
-            self.tweetText.append(self.clean_tweet(tweet.text).encode("utf-8"))
+            self.tweet_text.append(self.clean_tweet(tweet.text).encode("utf-8"))
             # print (tweet.text.translate(non_bmp_map)) 
+            
 
         # Write to csv and close csv file
-        csvWriter.writerow(self.tweetText)
-        csvFile.close()
+        csv_writer.writerow(self.tweet_text)
+        csv_file.close()
 
     def clean_tweet(self, tweet):
         # Remove Links, Special Characters etc from tweet
